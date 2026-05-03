2aa59f40d350967d723d2690e26ed76e4c21c448
Tudorthg <tudor.magurean06@e-uvt.ro>
2026-05-04 01:54:24 +0300

    Add files via upload

--- script.py
+++ script.py
@@ -1,7 +1,19 @@
 import random
-x = int(input("Input a number: "))
-f1 = open("numbers.txt", 'w')
-for i in range(0,x):
-    f1.write(str(random.randint(1,10000000)))
-    f1.write(" ")
-f1.close()
+import os
+x = int(input("Enter number of files to generate: "))
+A = []
+for i in range(x):
+    A.append(int(input("Number of elements in a file: ")))
+
+folder = "C:/Users/Tudor/PyCharmMiscProject/Numbers"
+os.makedirs(folder, exist_ok=True)
+
+for i in range(x):
+    filename = os.path.join(folder, f"numbers_{A[i]}.txt")
+    with open(filename, "w") as f:
+        for j in range(A[i]):
+            f.write(str(random.randint(0, 100000)))
+            f.write(" ")
+
+os.startfile(folder)
+
